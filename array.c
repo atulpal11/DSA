ccd887e5ae965cd3c98aceadb01ceb21620ee1ae
atulpal11 <atulpal8530@gmail.com>
2025-10-22 23:49:55 +0530

    Reversing an Array

--- array.c
+++ array.c
@@ -13,5 +13,19 @@ int main(){
     for(i=0;i<n;i++){
         printf("%d ",arr[i]);
     }
+    //To reverse the array
+    int j,temp;
+    for(i=0;i<n;i++){
+        for(j=n-1;j>i;j--){
+            temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+        }
+    }
+    //To Display an Array after reversing
+    printf("\nElements present in the array:");
+    for(i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
     return 0;
 }
