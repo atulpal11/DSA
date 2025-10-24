c1f33eb57728fef8003c598cb97a3240b0afad9e
atulpal11 <atulpal8530@gmail.com>
2025-10-24 05:33:16 +0530

    Nothing

---
+++ sorting.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int arr[100];
+int n;
+
+void getSize(){
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+}
+
+void inputArray() {
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+}
